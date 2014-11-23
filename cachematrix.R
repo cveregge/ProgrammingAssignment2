27227aaff384384396051dfe1ce7e0a052c36cc6
cveregge <cveregge@gmail.com>
2014-11-22 18:16:55 -0800

    Assignment 2 almost complete
    Need to prettify and remove testing code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#for testing clear workspace
+rm(list=ls())
 
-makeCacheMatrix <- function(x = matrix()) {
+#this is a matrix "object" with functions to get and set
+#the matrix or matrix inverse
 
+makeCacheMatrix <- function(m = matrix()) {
+  inv <- NULL
+  
+  #matrix setter function
+  set <- function(mat) {
+    m <<- mat
+    inv <<- NULL
+  }
+  
+  #matrix getter function
+  get <- function(){
+    m
+  }
+
+  #inverse getter function
+  getInverse <- function() { 
+    inv
+  }
+  
+  #inverse setter function
+  setInverse <- function(i){
+    inv <<- i
+  }
+  
+  #return the functions defined
+  list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+# calculates and returns the matrix inverse for "makeCacheMatrix"
+# if the inverse does not already exist
+# otherwise it returns the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  if(!is.null(inv)) {
+    message("Using cached inverse")
+    return(inv)
+  }
+  
+  #if the function hasn't returned yet then we need to calculate the inverse
+  m <- x$get()
+  
+  x$setInverse(solve(m))
+  
 }
+
+a <- makeCacheMatrix()
+
+a$set(matrix(1:4,c(2,2)))
+a$get()
+a$getInverse()
+cacheSolve(a)
+a$getInverse()
+cacheSolve(a)
+a$getInverse()
+
+a$get() %*% a$getInverse()
